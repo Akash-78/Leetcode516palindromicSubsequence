2f56a1d65297251a3734c396d97e4712abde7727
Akash-78 <akash.srivastava_cs19@gla.ac.in>
2022-07-30 23:31:55 +0530

    Initial commit

---
+++ src/Leetcode516.java
@@ -0,0 +1,25 @@
+public class Leetcode516 {
+    static int LongestPalindromicSubsequence(String s){
+        int n=s.length();
+        int[][] dp=new int[n][n];
+        for (int i=0;i<n;i++){
+            dp[i][i]=1;
+        }
+        for(int cur=2;cur<=n;cur++){
+            for (int i=0;i<n-cur+1;i++){
+                int j=i+cur-1;
+                if(s.charAt(i) == s.charAt(j)){
+                    dp[i][j]=dp[i+1][j-1]+2;
+                }
+                else {
+                    dp[i][j]=Math.max(dp[i+1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[0][n-1];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(LongestPalindromicSubsequence("bbbab"));
+    }
+}
